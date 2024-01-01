cca2a6a0ae06175582d1c5c87d9d7c5f563a6bf6
NabilNagib <nnaby@gmail.com>
2024-01-01 17:39:49 +0300

    button modified, Alert message & form adjustment

---
+++ Student Grade/grade.js
@@ -0,0 +1,23 @@
+function studentGrade() {
+  const marks = document.getElementById("marks").value;
+  const gradeNumber = parseFloat(marks);
+
+  let grade;
+  if (gradeNumber >= 80 && gradeNumber <= 100) {
+    grade = "Grade A";
+  } else if (gradeNumber >= 60 && gradeNumber <= 79) {
+    grade = "Grade B";
+  } else if (gradeNumber >= 50 && gradeNumber <= 59) {
+    grade = "Grade C";
+  } else if (gradeNumber >= 40 && gradeNumber <= 49) {
+    grade = "Grade D";
+  } else if (gradeNumber >= 0 && gradeNumber < 40) {
+    grade = "Grade E";
+  } else {
+    alert("Enter a number between 0 & 100");
+    return;
+  }
+
+  const grading = document.getElementById("grading");
+  grading.innerHTML = grade;
+}
