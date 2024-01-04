ee04cc2c8807b3b3990a4c75a831a534d8292b54
maruands <maruands@gmail.com>
2024-01-04 07:56:01 +0300

    adding tax code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function carSpeed() {
     const speed = document.getElementById("carSpeed").value;
-    
+    const okay = document.getElementById("points").hidden;
     if(speed <= 70){
         alert("Ok");
     } else {
@@ -13,5 +13,23 @@ function carSpeed() {
             
         }
         alert("Demerit points : " + points);
+    }
+}
+
+function calculatePayee() {
+    
+    const year = document.getElementById("year").value;
+    const month = document.getElementById("month").value;
+    const gross = document.getElementById("gross").value;
+    const benefit = document.getElementById("benefit").value;
+    alert("gross is: " + gross + "  benefit is: " + benefit);
+
+}
+
+function taxation(income){
+    if(income > 0 && income <=288000){
+        var tax1 = 288000 * 0.1
+        income = income - 280000
+        var tax2 = 100000 * 0.25
     }
 }
