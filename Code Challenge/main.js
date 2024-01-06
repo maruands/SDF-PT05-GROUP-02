bcba24716602b3b15f38401f7f29565526bb7fbf
NabilNagib <nnaby@gmail.com>
2024-01-06 19:21:53 +0300

    overhaul of student grade UI, merged speed detector code

--- Code Challenge/main.js
+++ Code Challenge/main.js
@@ -29,25 +29,39 @@ let totalPoints = 0;
 function calculatePoints() {
   let speed = parseFloat(document.getElementById("inputNumber").value);
   let speedLimit = 70;
-
+  updateSpeedDisplay();
   if (isNaN(speed)) {
     alert("Enter Valid Number");
     return;
   }
 
   let points = Math.round((speed - speedLimit) / 5);
+  const message = document.getElementById("message");
   if (speed <= speedLimit) {
-    alert("OK");
+    //alert("OK");
+    message.innerHTML = "OK";
   } else {
     totalPoints += points;
 
     if (totalPoints > 12) {
-      alert("Licence Suspended!");
+      //alert("Licence Suspended!");
+      message.innerHTML = "Licence Suspended!";
       totalPoints = 0;
     } else {
-      alert("You have " + totalPoints + " points!");
+      //alert("You have " + totalPoints + " points!");
+      message.innerHTML = "You have " + totalPoints + " points!";
     }
   }
+}
+
+// Function to update the speed display dashboard
+function updateSpeedDisplay() {
+  
+  let meterBar = document.getElementById("inputNumber").value;
+  let speed = Math.round(meterBar);
+  
+  let speedDisplay = document.getElementById("speed");
+  speedDisplay.textContent = speed;
 }
 
 
